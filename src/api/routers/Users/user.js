4f4cbdfdd5681d428fa81594fa24f8e65e352bbd
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-02-27 22:38:24 -0300

    feat: Implementa 'userRouter.get' para a rota /user

--- src/api/routers/Users/user.js
+++ src/api/routers/Users/user.js
@@ -5,9 +5,11 @@ const userController = require('../../controllers/Users/user');
 
 const { createUser } = require('../../schemas/JoiSchemas/UserSchemas/userSchema');
 const validSchemas = require('../../schemas/middlewares/validSchemas');
+const validToken = require('../../auth/middlewares/validToken');
 
 const userRouter = express.Router();
 
 userRouter.post('/', validSchemas(createUser), rescue(userController.createUser));
+userRouter.get('/', validToken.verifyToken, rescue(userController.findAllUser));
 
 module.exports = userRouter;
